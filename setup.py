28e668f3e34b3940907f77bc62c237a4775409c3
Carlos Ríos V <crosvera@gmail.com>
2017-08-13 23:40:42 -0300

    added config_spec

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mediagoblin-restrict",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     include_package_datas=True,
     install_requires=[],
