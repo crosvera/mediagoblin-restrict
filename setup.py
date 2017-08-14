7c616cf9c2fd0b4095a0e90b52189222a81e9d20
Carlos Ríos V <crosvera@gmail.com>
2017-08-13 23:56:44 -0300

    fixing setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="mediagoblin-restrict",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
-    include_package_datas=True,
+    include_package_data=True,
     install_requires=[],
     license="AGPLv3"
 )
